e898ab1c39f9ca48ef4c059713364732487d8a87
ositodobbie10 <caamledzeppelin@gmail.com>
2024-07-05 22:52:01 -0600

    'Pase post noche'

--- src/Pages/PostPaseNoche.jsx
+++ src/Pages/PostPaseNoche.jsx
@@ -1,9 +1,23 @@
-import {Container,CoverParticles,PageTransition} from "../index" 
+import {Container,CoverParticles,PageTransition,
+        TransitionComponent,UseGlobal,FormularioEntrada,Tabla} from "../index" 
 const PostPaseNoche = () => {
+  const {ObjetoPostFacturacionform,GetObjetoPostFacturacionform} = UseGlobal();
+  const {FechaPostFacturacion} = ObjetoPostFacturacionform;
   return (
     <Container>
     <PageTransition/>
     <CoverParticles/>
+    <TransitionComponent position='bottom' className="w-full h-full flex flex-col gap-3">
+    <FormularioEntrada title='POST FACTURACION' 
+                       p1='Pantalla Muestra lo Facturado despues de Pase Batch'
+                       p2='Introduzca la fecha del PASE BATCH (YYMMDD)'
+                       value='FechaPostFacturacion'
+                       funcion1={GetObjetoPostFacturacionform}
+                       />
+    <div className="w-full">
+    <Tabla/>
+    </div>
+    </TransitionComponent>
     </Container>
   )
 }
